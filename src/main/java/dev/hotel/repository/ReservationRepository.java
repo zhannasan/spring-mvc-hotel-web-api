12d0580766cc3cdd4dfae3813dd3e49ce0a4d356
Zhanna <j.santybay@gmail.com>
2020-02-03 05:39:23 +0100

    Add hotel-app-console

--- src/main/java/dev/hotel/repository/ReservationRepository.java
+++ src/main/java/dev/hotel/repository/ReservationRepository.java
@@ -10,8 +10,9 @@ import dev.hotel.entite.Reservation;
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
 
-	// @Query(value = "SELECT v.Date FROM Reservation v WHERE NOT EXISTS(SELECT
-	// r.chambre_uuid FROM Reservation_chambre r WHERE c.uuid =
-	// r.chambre_uuid)", nativeQuery = true)
-	// List<String> existsNumeroNotLike();
+	/*
+	 * @Query(value = "SELECT v.Date FROM Reservation v WHERE NOT EXISTS(SELECT
+	 * r.chambre_uuid FROM Reservation_chambre r WHERE c.uuid =
+	 * r.chambre_uuid)", nativeQuery = true) List<String> existsNumeroNotLike();
+	 */
 }
